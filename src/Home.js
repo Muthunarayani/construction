f467d2cec861573e5ad85e4a9fe194ae58c3e364
muthu narayani <muthunarayanis.ug.21.cs@francisxavier.ac.in>
2023-09-19 20:57:02 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,173 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    
+    <><div className='bg-[url("https://as1.ftcdn.net/v2/jpg/00/95/29/68/1000_F_95296888_VeBf4npRMNJkIngif20ckHsaKXLTaE5w.jpg")] h-96 w-full bg-cover bg-center p-24'>
+
+      <h1 className='text-yellow-300 text-6xl font-bold text-left '>We are builders</h1>
+      <h2 className='text-black text-6xl font-light text-left mt-5'>We will build your dreams</h2>
+      <div class='p-5 flex gap-10'>
+        <div className='row '>
+
+          <button class='bg-yellow-300 px-10 py-5 text-xl uppercase tracking-widest text-white font-bold'>projects</button>
+          </div>
+          <button class='border-solid  border-white border-4 px-10 py-4 text-xl text-white uppercase tracking-widest'>About Us</button>
+        </div>
+      </div>
+    
+    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2">
+      <div>
+        <p class="mb-10 text-yellow-300 text-4xl font-bold text-center pt-20 pl-5">Welcome to </p>
+        <p class="mb-10 text-black-300 text-4xl font-bold text-center pl-5">EVLO CONSTRUCTION</p>
+        <hr class="w-48 h-1 mx-auto my-4 bg-yellow-300 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
+        <p class="mb-10 text-black-300 text-2xl font-light text-left pl-5">Construction companies often employ skilled laborers, engineers, architects, and other professionals to complete projects efficiently and to a high standard of quality.</p>
+        </div>
+           <div className='col'>
+            <div>
+    <p class="mb-10 text-black-300 text-2xl font-light text-left pt-20 pl-5">At EVLO construction, we are more than just builders; we are creators of spaces where dreams come to life. With a legacy of excellence spanning 5 years, we have established ourselves as a trusted and innovative construction company. Our commitment to delivering quality, safety, and sustainability is at the core of everything we do. From groundbreaking to project completion, we bring expertise, passion, and a relentless pursuit of perfection to every construction endeavor. </p>
+    </div>
+    <div>
+
+<button class='bg-yellow-300 px-10 py-5 text-xl uppercase tracking-widest mr-10 text-white font-bold'>read more</button>
+</div> 
+</div> 
+</div>
+    <div className='p-5 flex  w-full'>  
+    <div className='bg-[url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQX4OWJ4TDpcGK3dXw4At1p2BEEW1mnExE3ot0lLGoxnCchc66M9rD-Xr5PsypM_N8dAXE&usqp=CAU")] h-100  bg-cover contrast-50 bg-center p-24 '>
+
+<h1 className='text-white text-2xl font-bold text-left uppercase'>Design and building</h1>
+<hr class="w-48 h-1 mx-auto my-4 bg-yellow-300 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
+<h2 className='text-white text-2xl text-justify mt-5'>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</h2>
+    
+    </div>
+    <div className='bg-[url("https://media.istockphoto.com/id/1384317531/photo/before-and-after-of-man-painting-roller-to-reveal-newly-remodeled-room-with-fresh-light-green.jpg?s=612x612&w=0&k=20&c=wF448uWLu7btrsbZedGiIHqHbeu6KxA-YxZOiTkfcMY=")] h-100 bg-center contrast-50 bg-cover  p-24'>
+
+<h1 className='text-white text-2xl font-bold text-left uppercase'>Construction and innovation</h1>
+<hr class="w-48 h-1 mx-auto my-4 bg-yellow-300 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
+<h2 className='text-white text-2xl text-justify mt-5'>As the construction industry continually evolves to adopt new technologies, materials, and practices to improve efficiency, sustainability, and safety.</h2>   
+    </div>  
+            
+    <div className='bg-[url("https://media.istockphoto.com/id/1195687574/photo/interior-view-of-a-house-under-construction.jpg?s=612x612&w=0&k=20&c=X-eZ0nNCBLHuVEWPJEzu-Q6WUFKOxeqGLyfnL4-GsNM=")] h-100 bg-center contrast-50 bg-cover p-24 '>
+
+<h1 className='text-white text-2xl font-bold text-left uppercase '>Finishing techniques</h1>
+<hr class="w-48 h-1 mx-auto my-4 bg-yellow-300 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
+<h2 className='text-white text-2xl text-justify mt-5'>The final touches and processes applied to a building or structure to enhance its aesthetics, functionality, and durability.</h2>
+    
+    </div>         
+</div>
+<p className='text-3xl text-center text-yellow-300 font-bold'>SPECIAL OFFERS</p>
+
+          <h1 className='text-5xl pt-5 text-center text-black font-bold'>OUR BEST SERVICE</h1>
+          <hr class="w-48 h-1 mx-auto my-4 bg-yellow-300 border-0 rounded md:my-10 dark:bg-gray-700"></hr>    
+          <div class="grid grid-cols-1 gap-2 sm:grid-cols-3 pt-5">
+          <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto mt-10">
+      <div className="w-full p-8 text-center">
+        <div className="flex ">
+                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z" />
+</svg>
+          </div>
+          <div>
+          <p class="mb-3 text-gray-500 dark:text-gray-400 mt-1 ml-8 text-justify">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+          </div>
+          </div>
+          </div>
+          </div>
+          <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto mt-10">
+      <div className="w-full p-8 text-center">
+        <div className="flex ">
+                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
+</svg>
+          </div>
+          <div>
+          <p class="mb-3 text-gray-500 dark:text-gray-400 mt-1 ml-8 text-justify">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+          </div>
+          </div>
+          </div>
+          </div>
+          <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto mt-10">
+      <div className="w-full p-8 text-center">
+        <div className="flex ">
+                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+</svg>
+          </div>
+          <div>
+          <p class="mb-3 text-gray-500 dark:text-gray-400 mt-1 ml-8 text-justify">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+          </div>
+          </div>
+          </div>
+          </div>       
+    </div>
+   <div class="grid grid-cols-1 gap-6 sm:grid-cols-3 pt-5">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto mt-10">
+      <div className="w-full p-8 text-center">
+        <div className="flex ">
+                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5l.415-.207a.75.75 0 011.085.67V10.5m0 0h6m-6 0h-1.5m1.5 0v5.438c0 .354.161.697.473.865a3.751 3.751 0 005.452-2.553c.083-.409-.263-.75-.68-.75h-.745M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+
+          </div>
+          <div>
+          <p class="mb-3 text-gray-500 dark:text-gray-400 mt-1 ml-8 text-justify">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+          </div>
+          </div>
+          </div>
+          </div>
+         
+          <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto mt-10">
+      <div className="w-full p-8 text-center">
+        <div className="flex ">
+                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+</svg>
+
+          </div>
+          <div>
+          <p class="mb-3 text-gray-500 dark:text-gray-400 mt-1 ml-8 text-justify">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+          </div>
+          </div>
+          </div>
+          </div>
+          <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto mt-10">
+      <div className="w-full p-8 text-center">
+        <div className="flex ">
+                <div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> 
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
+</svg>
+
+          </div>
+          <div>
+          <p class="mb-3 text-gray-500 dark:text-gray-400 mt-1 ml-8 text-justify">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+          </div>
+          </div>
+          </div>
+          </div>
+          
+          
+    </div>
+
+      
+      </>
+
+    
+    
+
+
+
+
+
+    
+  );
+}
+
+
+
